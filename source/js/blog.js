9e971398a16b7c8e40bbc2e3aa3c512fce0ff90d
赵飞 <zhaofei@youwei.com>
2016-05-18 12:04:22 +0800

    add side bar

--- source/js/blog.js
+++ source/js/blog.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
 
 // Navigation Scripts to Show Header on Scroll-Up
 jQuery(document).ready(function($) {
-    var MQL = 1170;
+    var MQL = 960;
 
     //primary navigation slide-in effect
     if ($(window).width() > MQL) {
